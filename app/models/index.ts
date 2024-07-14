0550ad46244f2fad18677b4ea58817632e3ef9a1
alexisperdomod <dropdibemol@gmail.com>
2024-07-13 21:20:55 -0500

    feat: add gutendex response related types

--- app/models/index.ts
+++ app/models/index.ts
@@ -17,3 +17,30 @@ export type Err = {
     errorType: string
     message?: string
 }
+/**
+ * Book model type from Gutendex API.
+ * */
+export interface BookAuthor {
+    name: string
+    birth_year: number
+    death_year: number
+}
+export interface Book {
+    id: number
+    title: string
+    authors: BookAuthor[]
+    subjects: string[]
+    bookshelves: string[]
+    languages: string[]
+    copyright: boolean
+    media_type: string
+    formats: { [key: string]: string }
+    download_count?: number
+}
+
+export interface GutendexResponse {
+    count: number
+    next: string
+    previous: string
+    results: Book[]
+}
