c08ba3423b1b0b60fc076c7ecf2afacf8481afad
Sixela <dropdibemol@gmail.com>
2024-08-24 17:12:34 -0500

    feat: update generic fetcher method

--- src/services/fecher.service.ts
+++ src/services/fecher.service.ts
@@ -1,23 +1,10 @@
-import { FetcherOptions, Err } from "../models"
+import { FetcherOptions, Err } from '../models'
 
 /**
- * fecher service to fetch data from diferent sources, recive the model of the type of response you are waiting for. 
+ * fecher service to fetch data from diferent sources, recive the model of the type of response you are waiting for.
  * */
-export const fecher = async <T>(url: string, options?: FetcherOptions): Promise<T | Err> => {
-    try {
-        const response = await fetch(url, options)
-        if (!response.ok)
-            return {
-                status: response.status,
-                errorType: response.statusText
-            }
-        return response.json() as Promise<T>
-    } catch (e) {
-        let err = e as Error
-        return {
-            status: 500,
-            errorType: 'Internal Server Error',
-            message: err.message
-        }
-    }
+export const fetcher = async <T>(url: string, options?: FetcherOptions): Promise<T> => {
+    const response = await fetch(url, options)
+    if (!response.ok) throw new Error('Error when fetching data', { cause: response.statusText })
+    return response.json() as Promise<T>
 }
