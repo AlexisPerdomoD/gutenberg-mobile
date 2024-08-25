05b961f9300f12c0e1fe8b46451c438735405d58
alexisperdomod <dropdibemol@gmail.com>
2024-08-25 00:12:52 -0500

    feat: agregar linter setup adecuado

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,9 @@
 import { View, Text } from 'react-native'
-
+import constants from 'expo-constants'
 // Components/header/index.tsx
 const Header = () => {
     return (
-        <View className="w-full p-2 justify-center items-center bg-alternateBackground">
+        <View style={{ paddingTop: constants.statusBarHeight }} className="w-full p-2 justify-center items-center bg-alternateBackground">
             <Text className="text-darkText font-textBold text-2xl text-center">Gutenberg</Text>
         </View>
     )
